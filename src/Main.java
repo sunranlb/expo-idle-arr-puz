cd0825ccd906ff96a4953fa17c1511ff64a9b768
sr <sunranlb@qq.com>
2022-11-20 21:23:23 +0800

    methods

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.*;
+
 public class Main {
+    private static Map<String, int[]> METHOD = new HashMap<String, int[]>() {{
+        put("01", new int[]{1, 0, -3, -1});
+        put("02", new int[]{2, 0, 0, -2});
+        put("03", new int[]{3, 0, 3, 3});
+        put("04", new int[]{-2, 0, 0, 2});
+        put("05", new int[]{-1, 0, 3, 1});
+        put("11", new int[]{0, 3, 3, 3});
+        put("21", new int[]{-2, 2, -3, 0});
+        put("22", new int[]{2, -2, 0, 0});
+        put("23", new int[]{0, 0, 3, 0});
+        put("24", new int[]{-2, 2, 0, 0});
+        put("25", new int[]{2, -2, 3, 0});
+        put("31", new int[]{1, -2, 0, 1});
+        put("32", new int[]{2, 2, 0, 2});
+        put("33", new int[]{3, 0, 0, 3});
+        put("34", new int[]{-2, -2, 0, -2});
+        put("35", new int[]{-1, 2, 0, -1});
+    }};
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
     }
 }
